f645dc4629768822e65eca2bd994492150c76c83
Manh Duong <manh.duong@opswat.com>
2023-06-06 08:31:13 +0700

    Add sample

---
+++ Sample/blockchain.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+# Simple blockchain implementation 
+
+# Part 1 - Building a Blockchain
+
+# Part 2 - Mining our Blockchain
+
+# Creating a Web App
+# Creating a Blockchain
+# Mining a new block
+# Getting the full Blockchain
+# Checking if the Blockchain is valid
+# Running the app
